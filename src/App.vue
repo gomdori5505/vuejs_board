54edcd7f15cc9e0b594792eba64c50a7e811238f
lsk <lsk@lskui-MacBookPro.local>
2020-12-31 17:14:18 +0900

    fix: add app bar

--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,11 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>LSK board</v-toolbar-title>
+    </v-app-bar>
 
     <v-main>
       <router-view />
